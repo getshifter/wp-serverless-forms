6aafe094a0d56fab1d0ed012fe6138f68b246daf
Daniel Olson <emaildano@gmail.com>
2018-02-18 14:17:31 -0500

    resolved cf7 script errors and added helper classes for submissions

--- assets/js/modules/wp-sls-forms.js
+++ assets/js/modules/wp-sls-forms.js
@@ -6,35 +6,49 @@
 
 // Target WP Provider class and add wp-sls-forms class
 export function wpServerlessFormsClass(form) {
- var $form = jQuery(form)
- $form.removeAttr('action method novalidate')
- $form.addClass('wp-sls-forms')
+  var $form = jQuery(form)
+  $form.removeAttr('action method novalidate')
+  $form.addClass('wp-sls-forms')
 
 
- jQuery(document).ready(
-   function($) {
+  jQuery(document).ready(
+    function($) {
 
-     var $form = jQuery(form)
-     $form.removeAttr('action method novalidate')
-     $form.addClass('wp-sls-forms')
+      var $form = jQuery(form)
+      $form.removeAttr('novalidate')
+      $form.attr('method', 'POST')
+      $form.attr('accept-charset', 'UTF-8')
+      $form.addClass('wp-sls-forms')
 
-   }
- );
+      $('<input>').attr({
+        type: 'hidden',
+        'name': 'send-from-url',
+        'value': window.location.href,
+      }).appendTo($form)
+
+      $('<input>').attr({
+        type: 'hidden',
+        'name': 'sent-from-title',
+        'value': document.title,
+      }).appendTo($form)
+
+    }
+  );
 }
 
 // Swap WP Provider Req fields to HTML5
 export function wpServerlessFormsRequied(form, field) {
 
- jQuery(document).ready(
-   function($) {
+  jQuery(document).ready(
+    function($) {
 
-     var $form = jQuery(form)
-     var $field = jQuery(field)
+      var $form = jQuery(form)
+      var $field = jQuery(field)
 
-     $form.find($field).each(function() {
-       $(this).attr('required', true)
-     })
+      $form.find($field).each(function() {
+        $(this).attr('required', true)
+      })
 
-   }
- );
+    }
+  );
 }
